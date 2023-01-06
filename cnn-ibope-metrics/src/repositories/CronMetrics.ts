2014e9ee8c1ef77339da2f26d5e51ec4cd1674db
Sergio Sposito <serginho.sposito@hotmail.com>
2023-01-06 12:21:49 -0300

    init cron

--- cnn-ibope-metrics/src/repositories/CronMetrics.ts
+++ cnn-ibope-metrics/src/repositories/CronMetrics.ts
@@ -50,6 +50,8 @@ class CronMetrics implements ICronMetrics {
         hour: "numeric",
         minute: "numeric"
       }));
+      this.CronRunBotIbope();
+      this.CronRunBotYoutube();
     });
   }
 }
