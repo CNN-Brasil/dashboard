b3a3814c663e930f30a1ea001afa7a7efaeceb32
Sergio Sposito <serginho.sposito@hotmail.com>
2023-01-16 16:04:36 -0300

    ajustment gi

--- cnn-ibope-metrics/src/useCase/jsonMetric/JsonMetricUseCase.ts
+++ cnn-ibope-metrics/src/useCase/jsonMetric/JsonMetricUseCase.ts
@@ -3,7 +3,7 @@ import { JsonMetricFS } from "../../repositories/JsonMetricFS";
 class JsonMetricUseCase {
     constructor(private jsonMetricUseCase: JsonMetricFS) {}
    
-    GetJson(archive: string): string {
+    GetJson(archive: string): object {
         const json = this.jsonMetricUseCase.GetJson(archive);
         return json;
     }
